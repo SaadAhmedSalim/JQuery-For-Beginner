6548adcd412cfad459e30da9bef080d31a9f1bc9
SaadSalim <shuvrohossain8@gmail.com>
2021-03-04 18:55:13 +0600

    updated

---
+++ JQueryProjects/cssMethod.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("button").click(function(){
+		$("h1,h2,p,div").css({"background-color": "green", "font-size": "200%"});
+		// $("h2,div").css({" background-color": "lime");
+	});
+});
