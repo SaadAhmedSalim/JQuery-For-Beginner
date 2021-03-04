1731dcd4402c5aa3f93bf6195b22996c00f2b3c0
SaadSalim <shuvrohossain8@gmail.com>
2021-03-04 12:48:08 +0600

    updated

--- JQueryProjects/getContentMethod.js
+++ JQueryProjects/getContentMethod.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
 	$("#btn1").click(function(){
 		alert("Text: " + $("#test").text());		
 	});
+
+	$("#btn2").click(function(){
+		alert("HTML: " + $("#test").html());		
+	});
 });
